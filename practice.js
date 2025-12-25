b68fa68c8601fb125b69ae879d7e4d59bea15a58
iramanaditya-svg <iramanaditya@gmail.com>
2025-12-25 14:28:46 +0530

    Update practice.js
    Removed Firebase initialization code and configuration.

--- practice.js
+++ practice.js
@@ -54,18 +54,6 @@ desertmap.addEventListener("click", () => {
     maingame(desert);
 });
 
-import {initializeApp} from 'firebase/app'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDaR2-_xIQTQfH0_UG-6VAh5H2YsqfbI2w",
-  authDomain: "tank-wars-19f87.firebaseapp.com",
-  projectId: "tank-wars-19f87",
-  storageBucket: "tank-wars-19f87.firebasestorage.app",
-  messagingSenderId: "159952933078",
-  appId: "1:159952933078:web:550a2d499b3ede8058b977"
-};
-
-initializeApp(firebaseConfig)
 
 function maingame(map) {
     let p1hasleft = false;
