dc8ca37a77255a497eca9b225924439c1aa7e0c4
iramanaditya-svg <iramanaditya@gmail.com>
2026-01-01 21:59:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const firebaseconfig = {
+    apiKey: "",
+    authDomain: "tank-wars-19f87.firebaseapp.com",
+    projectId: "tank-wars-19f87",
+    storageBucket: "tank-wars-19f87.firebasestorage.app",
+    messagingSenderId: "159952933078",
+    appId: "1:159952933078:web:550a2d499b3ede8058b977"
+};
+
+
+export default firebaseconfig;
